8ad1eb6248070e5adfa7abda0b9ce67ac1ee8fb2
Bolorchimeg <bolorchimeg.g@gmail.com>
2022-01-26 15:51:25 +1100

    Articles child component added inside app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import {Provider} from 'react-redux';
 import {store} from './actions/store';
+import Articles from './components/Articles';
 
 
 function App() {
   return (
-    <Provider store>
+    <Provider store={store}>
+      <Articles/>
     </Provider>
   );
 }
